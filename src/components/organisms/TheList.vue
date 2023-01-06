6b7469cd61ba0e45afad39a03a7314799efc94b7
rafael <rafaelcnunes@icloud.com>
2023-01-06 20:32:01 -0300

    added list

--- src/components/organisms/TheList.vue
+++ src/components/organisms/TheList.vue
@@ -1,15 +1,39 @@
 <template>
   <div>
-    <h1>Hello World</h1>
+    <TheTypography content="The List" tag="h1" />
+    <div id="list">
+      <div v-for="item in items" v-bind:key="item.id">
+        <TheItem v-bind:item="item.name" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "TheList",
+  data() {
+    return {
+      items: [],
+    };
+  },
+  created() {
+    this.getItems();
+  },
+  methods: {
+    getItems() {
+      localStorage.getItem("tasks") &&
+        (this.items = JSON.parse(localStorage.getItem("tasks")));
+    },
+  },
 };
 </script>
 
-<style scoped>
-/**/
+<style lang="scss" scoped>
+#list {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  border: 1px solid $color-primary-dark;
+}
 </style>
