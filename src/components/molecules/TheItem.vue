f6654b22fa2ad9225ae2b1e192e37d9a3f053a97
rafael <rafaelcnunes@icloud.com>
2023-01-06 20:31:39 -0300

    added item

--- src/components/molecules/TheItem.vue
+++ src/components/molecules/TheItem.vue
@@ -1,9 +1,21 @@
-<template></template>
+<template>
+  <div id="the_item">
+    <TheTypography v-bind:content="item" tag="span" />
+  </div>
+</template>
 
 <script>
 export default {
   name: "TheItem",
+  props: {
+    item: {
+      type: String,
+      required: true,
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+//
+</style>
