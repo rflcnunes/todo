68e1cac2fe5adca07f10cb8b891dd4d4cdb304a4
rafael <rafaelcnunes@icloud.com>
2023-01-06 20:29:56 -0300

    added components globally

--- src/main.js
+++ src/main.js
@@ -2,6 +2,22 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+// Atoms
+import TheButton from "@/components/atoms/TheButton.vue";
+import TheInput from "@/components/atoms/TheInput.vue";
+import TheTypography from "@/components/atoms/TheTypography.vue";
+
+Vue.component("TheButton", TheButton);
+Vue.component("TheInput", TheInput);
+Vue.component("TheTypography", TheTypography);
+
+// Molecules
+import TheItem from "@/components/molecules/TheItem.vue";
+import TheForm from "@/components/molecules/TheForm.vue";
+
+Vue.component("TheItem", TheItem);
+Vue.component("TheForm", TheForm);
+
 Vue.config.productionTip = false;
 
 new Vue({
