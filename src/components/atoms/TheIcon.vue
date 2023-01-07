ca550d5408121e26158aadd6bf763827c241c6d5
rafael <rafaelcnunes@icloud.com>
2023-01-07 04:33:23 -0300

    added material icons

---
+++ src/components/atoms/TheIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <span class="material-icons">{{ icon }}</span>
+</template>
+
+<script>
+export default {
+  name: "TheIcon",
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+/**/
+</style>
