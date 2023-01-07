b4ef2ad6c1623a769cd83dfb415a7b9aa5b0ff60
rafael <rafaelcnunes@icloud.com>
2023-01-07 01:23:18 -0300

    added checkItem method and adjustments in the css

--- src/components/organisms/TheList.vue
+++ src/components/organisms/TheList.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-    <TheTypography content="The List" tag="h1" />
+  <div id="the_list">
     <div id="list">
-      <div v-for="item in items" v-bind:key="item.id">
-        <TheItem v-bind:item="item.name" />
+      <div v-for="item in items" v-bind:key="item.id" id="item">
+        <TheItem v-bind:item="item.name" v-bind:checked="item.isFinished" />
+        <button v-on:click="checkItem(item.id)">Check</button>
       </div>
     </div>
   </div>
@@ -25,15 +25,47 @@ export default {
       localStorage.getItem("tasks") &&
         (this.items = JSON.parse(localStorage.getItem("tasks")));
     },
+    checkItem(id) {
+      this.items.forEach((item) => {
+        if (item.id === id) {
+          item.isFinished = !item.isFinished;
+        }
+      });
+      localStorage.setItem("tasks", JSON.stringify(this.items));
+      this.getItems();
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-#list {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  border: 1px solid $color-primary-dark;
+#the_list {
+  width: 100%;
+  margin-top: 10px;
+  border: 1px solid black;
+  border-radius: 20px;
+
+  #list {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 100%;
+    height: 100%;
+
+    #item {
+      border-bottom: 1px solid $color-primary-dark;
+      width: 97%;
+      height: 50px;
+      display: flex;
+      align-items: center;
+      flex-direction: row;
+      padding: 0 5px 0 5px;
+      justify-content: space-between;
+    }
+
+    #item:last-child {
+      border-bottom: none;
+    }
+  }
 }
 </style>
