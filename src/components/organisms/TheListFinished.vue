ed932070769481d22b8a7a2b0d1da58302754e33
rafael <rafaelcnunes@icloud.com>
2023-01-07 03:18:06 -0300

    added finished list

---
+++ src/components/organisms/TheListFinished.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="the_list_finished">
+    <div id="list">
+      <div v-for="item in items" v-bind:key="item.id" id="item">
+        <TheItem
+          v-bind:item="item.name"
+          v-bind:checked="item.isFinished"
+          v-bind:deleted="item.isDeleted"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheListFinished",
+  data() {
+    return {
+      items: [],
+    };
+  },
+  created() {
+    this.getItems();
+  },
+  methods: {
+    getItems() {
+      const tasks = localStorage.getItem("tasks");
+      JSON.parse(tasks).forEach((item) => {
+        if (item.isFinished && !item.isDeleted) {
+          this.items.push(item);
+        }
+      });
+
+      this.$emit("finish", this.items);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#the_list_finished {
+  width: 100%;
+  margin-top: 10px;
+  border: 1px solid black;
+  border-radius: 20px;
+
+  #list {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 100%;
+    height: 100%;
+
+    #item {
+      border-bottom: 1px solid $color-primary-dark;
+      width: 97%;
+      height: 50px;
+      display: flex;
+      align-items: center;
+      flex-direction: row;
+      padding: 0 5px 0 5px;
+      justify-content: space-between;
+    }
+
+    #item:last-child {
+      border-bottom: none;
+    }
+  }
+}
+</style>
